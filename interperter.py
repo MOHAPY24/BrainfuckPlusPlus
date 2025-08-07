592edd97ee891e0e9a839caae2316ffe10dbe9de
Mohammed Mahmoud <166178531+MOHAPY24@users.noreply.github.com>
2025-08-07 20:21:29 +0400

    Add files via upload

--- interperter.py
+++ interperter.py
@@ -5,102 +5,153 @@ import os
 import sys
 
 file_extensions = [".bfpp", ".bfa"]
-tape = [0] * 500
+tape = [0] * 3000
 pointer = 0
 code = ""
 variables = {}
+ver = 1.1
 
 
 if file_extensions[0] in sys.argv[1] or file_extensions[1] in sys.argv[1]:
     with open(sys.argv[1], "r") as fl:
-        print(sys.argv[1])
+        print(sys.argv[1] + f", version {ver}")
         content = fl.read()
         global comment_mode
         comment_mode = False
         content = content.strip()
         code = content
+        pc = 0
+        loop_stack = []
+
+        def bfpp_gen(text):
+            out = ""
+            for char in text:
+                out += "+" * ord(char) + ".>"
+            return out[:-1] + ","
+
+
         for line in code:
-            line.strip()
+            line = line.strip()
+
             if "$" in line:
                 comment_mode = True
                 continue
-            if "/" in line:
+            elif "/" in line:
                 comment_mode = False
-            if ">" in line:
+            elif ">" in line:
                 if comment_mode == True:
                     continue
                 else:
                     pointer += 1
-            if "<" in line:
+            elif "<" in line:
                 if comment_mode == True:
                     continue
                 else:
                     pointer -= 1
-            if "." in line:
+            elif "." in line:
                 if comment_mode == True:
                     continue
                 else:
-                    print(tape[pointer])
-            if "+" in line:
+                    print(chr(tape[pointer]), end='')
+            elif "+" in line:
                 if comment_mode == True:
                     continue
                 else:
                     tape[pointer] += 1
-            if "-" in line:
+            elif "-" in line:
                 if comment_mode == True:
                     continue
                 else:
                     tape[pointer] -= 1
-            if "@" in line:
+            elif "@" in line:
                 if comment_mode == True:
                     continue
                 else:
                     print(tape)
-                    tape.clear()
-                    tape = [""] * 250
-            if "," in line:
+            elif "," in line:
                 if comment_mode == True:
                     continue
                 else:
-                    quit()
-            if "," not in code:
-                raise Exception("SyntaxError: file closer , not found at EOF")
-            if "'" in line:
+                    break
+            elif "," not in code:
+                raise SyntaxError("SyntaxError: file closer , not found at EOF")
+            elif "'" in line:
                 if comment_mode == True:
                     continue
                 else:
                     if tape[0] > 1:
-                        Exception("RuntimeError: Cell 0 has information while trying to use function '")
+                        raise RuntimeError("RuntimeError: Cell 0 has information while trying to use function '")
                     else:
                         save = tape[pointer]
                         pointer = 0
                         tape[pointer] = save
-            if "|" in line:
+            elif "|" in line:
                 if comment_mode == True:
                     continue
                 else:
                     global savepoint
                     global saveval
                     saveval = tape[pointer] 
-            if "%" in line: 
+            elif "%" in line: 
                 if comment_mode == True:
                     continue
                 else:
                     tape[pointer] = saveval
+            elif "!" in line:
+                if comment_mode == True:
+                    continue
+                else:
+                    tape[pointer] = 0
 
+            elif "?" in line:
+                if comment_mode == True:
+                    continue
+                else:
+                    tape.clear()
+                    tape[0] * 3000
 
-            if "#" in line:
+            elif "_" in line:
+                continue
+
+            elif "^" in line:
+                if comment_mode == True:
+                    continue
+                else:
+                    i = input(": ")
+                    if int(i) is int:
+                        tape[pointer] = i
+                    else:
+                        raise ValueError(f"Argument {i} is not a valid inttype.")
+
+            elif "#" in line:
                 if comment_mode == True:
                     continue
                 else:
                     save = tape[pointer - 1]
                     tape[pointer] = save
             
-            if "*" in line:
+            elif "*" in line:
                 if comment_mode == True:
                     continue
                 else:
                     tape[pointer] = tape[pointer] * tape[pointer]
+
+            elif ":" in line:
+                if comment_mode == True:
+                    print(tape[pointer])
+
+            elif "=" in line:
+                if comment_mode == True:
+                    tape[pointer] = tape[pointer + 1]
+
+            elif pointer > 3000:
+                raise MemoryError("Pointer has exceeded tape maximum size.")
+
+            else:
+               raise SyntaxError(f"Character {line} is not defined or a valid argument.")
+
+
+
 
 else:
     Exception("RuntimeError: No file found.")
